bf59c1442421e6176a4f0c314fd2e1d031cafcf7
adina <alew3@student.touro.edu>
2025-05-08 16:28:16 -0400

    created session, flashcards, quizzes

--- core/models.py
+++ core/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class StudySession(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    uploaded_file = models.FileField(upload_to='uploads/')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.title} - {self.user.username}"
+
+class Flashcard(models.Model):
+    session = models.ForeignKey(StudySession, on_delete=models.CASCADE, related_name='flashcards')
+    front = models.CharField(max_length=255)  # e.g. question
+    back = models.TextField()                 # e.g. answer
+
+    def __str__(self):
+        return f"{self.front[:30]}..."
+
+class QuizQuestion(models.Model):
+    session = models.ForeignKey(StudySession, on_delete=models.CASCADE, related_name='quiz_questions')
+    question = models.TextField()
+    correct_answer = models.CharField(max_length=255)
+    user_answer = models.CharField(max_length=255, blank=True)
+
+    def __str__(self):
+        return f"{self.question[:50]}..."
