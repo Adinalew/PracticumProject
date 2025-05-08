3f65df03fd37c172d7f2b0f5429979d9b6292a26
MinnieEisenstein <meisenst3@student.touro.edu>
2025-05-08 16:25:36 -0400

    added a register page and linked it to login page

--- core/urls.py
+++ core/urls.py
@@ -1,3 +1,4 @@
+from . import views
 from django.urls import path
 from django.contrib.auth import views as auth_views
 from .views import home_view
@@ -5,4 +6,7 @@ from .views import home_view
 urlpatterns = [
     path('', home_view, name='home'),
     path('login/', auth_views.LoginView.as_view(), name='login'),
+    path('register/', views.register_view, name='register'),  # URL for the sign-up page
+
+
 ]
--- core/views.py
+++ core/views.py
@@ -1,4 +1,16 @@
 from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import redirect
 
 def home_view(request):
     return render(request, 'home.html')  # Render your homepage HTML
+
+def register_view(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')  # Redirect to login page after successful registration
+    else:
+        form = UserCreationForm()
+    return render(request, 'register.html', {'form': form})
